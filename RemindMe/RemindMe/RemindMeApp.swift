b916a58782273a579629e915efc35fa75378795b
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2024-03-21 21:42:53 +0100

    Initial commit

---
+++ RemindMe/RemindMe/RemindMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindMeApp.swift
+//  RemindMe
+//
+//  Created by Kamil Wójcicki on 21/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct RemindMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
