b4ba8d8052dd3f7a1a20b5b7f9467f7cbf90cc18
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2024-04-05 22:12:42 +0200

    Add Navigation package

---
+++ Dependencies/Navigation/Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Navigation",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        .library(
+            name: "Navigation",
+            targets: ["Navigation"]),
+    ],
+    dependencies: [
+        .package(path: "../DependencyInjection")
+    ],
+    targets: [
+        .target(
+            name: "NavigationInterface",
+            dependencies: [
+                .product(
+                    name: "DependencyInjection",
+                    package: "DependencyInjection"
+                )
+            ]
+        ),
+        .target(
+            name: "Navigation",
+            dependencies: [
+                "NavigationInterface"
+            ]
+        ),
+        .testTarget(
+            name: "NavigationTests",
+            dependencies: ["Navigation"]),
+    ]
+)
---
+++ Dependencies/Navigation/Sources/NavigationInterface/NavigationInterface.swift
@@ -0,0 +1,8 @@
+//
+//  NavigationInterface.swift
+//  
+//
+//  Created by Kamil Wójcicki on 05/04/2024.
+//
+
+import Foundation
