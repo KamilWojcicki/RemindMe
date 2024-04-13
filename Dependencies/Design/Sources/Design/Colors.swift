3c11709fc953aa89975f4b7883638ea40711fac0
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2024-04-13 12:41:39 +0200

    Create Design Package and add Colors

---
+++ Dependencies/Design/Sources/Design/Colors.swift
@@ -0,0 +1,20 @@
+//
+//  Colors.swift
+//
+//
+//  Created by Kamil Wójcicki on 13/04/2024.
+//
+
+import SwiftUI
+
+public struct Colors {
+    public static let blue = Color("Blue", bundle: .module)
+    
+    public static let ghostWhite = Color("GhostWhite", bundle: .module)
+    
+    public static let imperialRed = Color("ImperialRed", bundle: .module)
+    
+    public static let mantis = Color("Mantis", bundle: .module)
+
+    public static let vistaBlue = Color("VistaBlue", bundle: .module)
+}
