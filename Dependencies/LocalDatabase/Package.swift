d38a66ad53f42e7a7ff626ca8d985d92be85dbb5
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2024-04-18 20:42:19 +0200

    Create database functions to manage objects

---
+++ Dependencies/LocalDatabase/Package.swift
@@ -0,0 +1,49 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "LocalDatabase",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        .library(
+            name: "LocalDatabase",
+            targets: ["LocalDatabase"]
+        ),
+        .library(
+            name: "LocalDatabaseInterface",
+            targets: ["LocalDatabaseInterface"]
+        )
+    ],
+    dependencies: [
+        .package(path: "../DependencyInjection"),
+        .package(url: "https://github.com/realm/realm-swift.git", exact: "10.49.1")
+    ],
+    targets: [
+        .target(
+            name: "LocalDatabaseInterface",
+            dependencies: [
+                .product(
+                    name: "RealmSwift",
+                    package: "realm-swift"
+                )
+            ]
+        ),
+        .target(
+            name: "LocalDatabase",
+            dependencies: [
+                "LocalDatabaseInterface",
+                .product(
+                    name: "DependencyInjection",
+                    package: "DependencyInjection"
+                )
+            ]
+        ),
+        .testTarget(
+            name: "LocalDatabaseTests",
+            dependencies: ["LocalDatabase"]
+        )
+    ]
+)
---
+++ Dependencies/LocalDatabase/Sources/LocalDatabaseInterface/LocalDatabaseInterface.swift
@@ -0,0 +1,38 @@
+//
+//  LocalDatabaseInterface.swift
+//  
+//
+//  Created by Kamil Wójcicki on 14/04/2024.
+//
+
+import Foundation
+import RealmSwift
+
+public protocol LocalDAOInterface: Object {
+    associatedtype LocalModel: LocalStorable
+    init(from: LocalModel)
+}
+
+public protocol LocalStorable: Codable, Equatable {
+    associatedtype LocalDAO: LocalDAOInterface
+    init(from: LocalDAO)
+}
+
+public protocol LocalDatabaseManagerInterface {
+    func create<Object: LocalStorable>(_ object: Object) async throws
+    func create<Object: LocalStorable>(_ objects: [Object]) async throws
+    func read<Object: LocalStorable>(type: Object.Type, primaryKey: String) async throws -> Object
+    func read<Object: LocalStorable>() async throws -> [Object]
+    func update<Object: LocalStorable>(type: Object.Type, withUpdates updates: [String : Any]) async throws
+    func delete<Object: LocalStorable>(type: Object.Type, primaryKey: String) async throws
+    func deleteAllWithSpecificType<Object: LocalStorable>(type: Object.Type) async throws
+    func deleteAll() async throws
+}
+
+public enum LocalDatabaseManagerError: Error {
+    case unableToInitializeRealm
+    case unableToCreate
+    case unableToRead
+    case unableToUpdate
+    case unableToDelete
+}
