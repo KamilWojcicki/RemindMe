08ef9a657670873f29118d908f020cfb90e5d991
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2024-04-18 22:15:26 +0200

    Add ToDoManager to assemblies

--- Dependencies/ToDo/Sources/ToDo/ToDo.swift
+++ Dependencies/ToDo/Sources/ToDo/ToDo.swift
@@ -3,10 +3,12 @@
 
 import DependencyInjection
 import LocalDatabase
+import ToDoInterface
 
 public struct Dependencies {
     public static func inject() {
         //TODO: inject assemblies of ToDo Manager
+        Assemblies.inject(type: ToDoManagerInterface.self, object: ToDoManager())
         
         LocalDatabase.Dependencies.inject()
     }
