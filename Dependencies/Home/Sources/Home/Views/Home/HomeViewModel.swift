c1c8f022017001d9151e3c3f94b324475d3e5c90
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2024-05-10 10:19:11 +0200

    Add test HomeView to Home Package

--- Dependencies/Home/Sources/Home/Views/Home/HomeViewModel.swift
+++ Dependencies/Home/Sources/Home/Views/Home/HomeViewModel.swift
@@ -1,8 +1,12 @@
 //
-//  File.swift
-//  
+//  HomeViewModel.swift
+//
 //
 //  Created by Kamil Wójcicki on 10/05/2024.
 //
 
 import Foundation
+
+final class HomeViewModel: ObservableObject {
+    @Published var isEdited: Bool = false
+}
