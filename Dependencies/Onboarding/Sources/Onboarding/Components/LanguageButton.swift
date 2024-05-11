3b380e54f27e225fc9bc88bebf64b191446b5e76
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2024-05-11 09:52:23 +0200

    Add LanugageButton component

--- Dependencies/Onboarding/Sources/Onboarding/Components/LanguageButton.swift
+++ Dependencies/Onboarding/Sources/Onboarding/Components/LanguageButton.swift
@@ -1,18 +1,39 @@
 //
-//  SwiftUIView.swift
-//  
+//  LanguageButton.swift
+//
 //
 //  Created by Kamil Wójcicki on 10/05/2024.
 //
 
+import Design
+import Localizations
 import SwiftUI
 
-struct SwiftUIView: View {
+struct LanguageButton: View {
+    private var currentLanguage: SupportedLanguage
+    private var text: String
+    @Binding var selectedLanguage: SupportedLanguage
+
+    init(currentLanguage: SupportedLanguage, text: String, selectedLanguage: Binding<SupportedLanguage>) {
+        self.currentLanguage = currentLanguage
+        self.text = text
+        self._selectedLanguage = selectedLanguage
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            selectedLanguage = currentLanguage
+        } label: {
+            Text(text)
+        }
+        .shadow(
+            color: Colors.night.opacity(0.55),
+            radius: 10
+        )
+        .font(.largeTitle)
     }
 }
 
 #Preview {
-    SwiftUIView()
+    LanguageButton(currentLanguage: .english, text: "🇵🇱", selectedLanguage: .constant(.english))
 }
