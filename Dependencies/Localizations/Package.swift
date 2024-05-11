1126071c59654522c47a594c7c0cfe63da63129b
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2024-05-11 09:51:43 +0200

    Add Localizations Package and logic to change language in app

--- Dependencies/Localizations/Package.swift
+++ Dependencies/Localizations/Package.swift
@@ -5,19 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "Localizations",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v17)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Localizations",
-            targets: ["Localizations"]),
+            targets: ["Localizations"]
+        ),
+    ],
+    dependencies: [
+        
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Localizations"),
-        .testTarget(
-            name: "LocalizationsTests",
-            dependencies: ["Localizations"]),
+            name: "Localizations",
+            dependencies: [],
+            resources: [.process("Resources/")]
+        )
     ]
 )
--- Dependencies/Localizations/Sources/Localizations/Localizations.swift
+++ Dependencies/Localizations/Sources/Localizations/Localizations.swift
@@ -1,2 +1,56 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+enum Constants: String {
+    case languageKey = "language-key"
+    case appleLanguages = "AppleLanguages"
+}
+
+public enum SupportedLanguage: String {
+    case english = "english"
+    case polish = "polish"
+}
+
+extension String {
+    public var localized: String {
+        NSLocalizedString(self, bundle: .module, value: self, comment: "")
+    }
+}
+
+public class LanguageSetting: ObservableObject {
+    
+    public init() { 
+        setupInitialLocale()
+    }
+    
+    public var locale: Locale = .current {
+        didSet {
+            
+        }
+    }
+    
+    private func setupInitialLocale() {
+        if let language = UserDefaults.standard.string(forKey: Constants.languageKey.rawValue), let value = SupportedLanguage(rawValue: language) {
+            setLocale(language: value)
+        } else {
+            guard let languages = UserDefaults.standard.array(forKey: Constants.appleLanguages.rawValue), let currentLanguage = languages.first as? String else { return }
+            if currentLanguage.contains("pl") {
+                setLocale(language: .polish)
+            } else {
+                setLocale(language: .english)
+            }
+        }
+    }
+    
+    public func setLocale(language: SupportedLanguage) {
+        switch language {
+        case .english:
+            locale = Locale(identifier: "en")
+            UserDefaults.standard.setValue(SupportedLanguage.english.rawValue, forKey: Constants.languageKey.rawValue)
+            UserDefaults.standard.synchronize()
+        case .polish:
+            locale = Locale(identifier: "pl")
+            UserDefaults.standard.setValue(SupportedLanguage.polish.rawValue, forKey: Constants.languageKey.rawValue)
+            UserDefaults.standard.synchronize()
+        }
+    }
+}
