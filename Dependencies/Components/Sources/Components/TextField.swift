ac7ae4ab06f70caf70239692ccdf669b8a4ac80b
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2024-05-20 12:45:32 +0200

    Add TextField Component

--- Dependencies/Components/Sources/Components/TextField.swift
+++ Dependencies/Components/Sources/Components/TextField.swift
@@ -5,14 +5,30 @@
 //  Created by Kamil Wójcicki on 18/05/2024.
 //
 
+import Design
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct TextField: View {
+    @Binding private var textFieldLogin: String
+    
+    public init(textFieldLogin: Binding<String>) {
+        self._textFieldLogin = textFieldLogin
+    }
+    
+    public var body: some View {
+        VStack(spacing: 10) {
+            SwiftUI.TextField("", text: $textFieldLogin)
+                .font(.system(size: 23, weight: .bold, design: .default))
+                .foregroundStyle(Colors.ghostWhite)
+            
+        }
     }
 }
 
 #Preview {
-    SwiftUIView()
+    ZStack {
+        Colors.background().ignoresSafeArea()
+        TextField(textFieldLogin: .constant(""))
+    }
+
 }
