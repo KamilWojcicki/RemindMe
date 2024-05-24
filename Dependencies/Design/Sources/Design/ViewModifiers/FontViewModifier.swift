f328f9e18054de780c0f1ecc60f8f622f919b81a
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2024-05-24 10:15:53 +0200

    Add foregroundColor to FontViewModifier

--- Dependencies/Design/Sources/Design/ViewModifiers/FontViewModifier.swift
+++ Dependencies/Design/Sources/Design/ViewModifiers/FontViewModifier.swift
@@ -5,19 +5,24 @@
 //  Created by Kamil Wójcicki on 23/05/2024.
 //
 
+import Design
 import Foundation
 import SwiftUI
 
 struct FontViewModifier: ViewModifier {
+    
+    let foregroundColor: Color
+    
     func body(content: Content) -> some View {
         content
             .font(.size15Default)
+            .foregroundStyle(foregroundColor)
             .opacity(0.5)
     }
 }
 
 extension View {
-    public func withOpacityFont() -> some View {
-        modifier(FontViewModifier())
+    public func withOpacityFont(foregroundColor: Color = Colors.night) -> some View {
+        modifier(FontViewModifier(foregroundColor: foregroundColor))
     }
 }
