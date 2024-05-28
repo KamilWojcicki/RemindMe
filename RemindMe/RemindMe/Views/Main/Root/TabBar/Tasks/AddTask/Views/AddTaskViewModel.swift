20b9dbb2cf52ae79aee7cf61fda903b95f8df3bf
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2024-05-28 19:44:31 +0200

    Add some publishers and addTask function

--- RemindMe/RemindMe/Views/Main/Root/TabBar/Tasks/AddTask/Views/AddTaskViewModel.swift
+++ RemindMe/RemindMe/Views/Main/Root/TabBar/Tasks/AddTask/Views/AddTaskViewModel.swift
@@ -13,9 +13,16 @@ import ToDoInterface
 @MainActor
 final class AddTaskViewModel: ObservableObject {
     @Inject private var toDoManager: ToDoManagerInterface
+    @Published var startTimeTitle: String = ""
+    @Published var endTimeTitle: String?
+    @Published var description: String = ""
     @Published var selectDate: Date = Date()
     @Published var titleTextFieldText: String = ""
     @Published var showCategories: Bool = false
+    @Published var selectStartTime: Date = Date()
+    @Published var selectEndTime: Date = Date()
+    @Published var descriptionTextFieldText: String = ""
+    @Published var numberOfNotifications: Int = 1
     
     func showCategoryPickerView() {
         withAnimation {
@@ -26,6 +33,17 @@ final class AddTaskViewModel: ObservableObject {
     func showCategoryPickerWhenCategoryIsNil(using secViewModel: TabBarViewModel) {
         if secViewModel.toDoCategory == nil {
             showCategoryPickerView()
+        }
+    }
+    
+    func addTask(category: Categories) {
+        Task {
+            do {
+                let newToDo = ToDo(category: category, name: titleTextFieldText, toDoDescription: descriptionTextFieldText, executedTime: selectDate, numbersOfReminders: numberOfNotifications)
+                try await toDoManager.createToDo(todo: newToDo)
+            } catch {
+                print(error.localizedDescription)
+            }
         }
     }
 }
