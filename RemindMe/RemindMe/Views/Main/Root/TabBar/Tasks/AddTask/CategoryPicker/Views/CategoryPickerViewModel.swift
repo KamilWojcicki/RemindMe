7c4a2f523b6cda11f6a3588ddfd47a33f389c3ae
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2024-06-23 16:37:22 +0200

    Delete CategoryPickerViewModel

--- RemindMe/RemindMe/Views/Main/Root/TabBar/Tasks/AddTask/CategoryPicker/Views/CategoryPickerViewModel.swift
+++ RemindMe/RemindMe/Views/Main/Root/TabBar/Tasks/AddTask/CategoryPicker/Views/CategoryPickerViewModel.swift
@@ -5,4 +5,10 @@
 //  Created by Kamil Wójcicki on 12/06/2024.
 //
 
+import DependencyInjection
 import Foundation
+import SwiftUI
+import ToDoInterface
+
+final class CategoryPickerViewModel: ObservableObject {
+}
