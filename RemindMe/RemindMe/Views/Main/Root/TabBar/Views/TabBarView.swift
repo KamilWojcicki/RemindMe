496548f15e7094f6d57a925987b8c202108744ed
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2024-08-07 03:37:12 +0200

    Comment code for testing

--- RemindMe/RemindMe/Views/Main/Root/TabBar/Views/TabBarView.swift
+++ RemindMe/RemindMe/Views/Main/Root/TabBar/Views/TabBarView.swift
@@ -39,7 +39,7 @@ public struct TabBarView: View {
                                     .ignoresSafeArea()
                                     
                                 tab.rootView
-                                    .clipShape(.rect(bottomLeadingRadius: 45, bottomTrailingRadius: 45))
+//                                    .clipShape(.rect(bottomLeadingRadius: 45, bottomTrailingRadius: 45))
                             }
                         }
                     }
