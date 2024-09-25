83e0f7812a619403a2aac5816336c87859904eae
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2024-09-25 11:45:18 +0200

    Add symbols to Symbols struct

--- Dependencies/Design/Sources/Design/Symbols.swift
+++ Dependencies/Design/Sources/Design/Symbols.swift
@@ -6,24 +6,32 @@
 //
 
 import Foundation
+import SwiftUI
 
 public struct Symbols {
-    public static let checkmarkSeal = "checkmark.seal"
-    public static let checkmarkSealFill = "checkmark.seal.fill"
-    public static let pencilAndListClipboard = "pencil.and.list.clipboard"
-    public static let minusCircle = "minus.circle"
-    public static let docBadgeClock = "doc.badge.clock"
-    public static let checklist = "checklist"
-    public static let giftFill = "gift.fill"
-    public static let carFill = "car.fill"
-    public static let crossCircleFill = "cross.circle.fill"
-    public static let plusCircle = "plus.circle"
-    public static let magnifyingGlass = "magnifyingglass"
-    public static let arrowLeft = "arrow.left"
-    public static let line3Horizontal = "line.3.horizontal"
-    public static let airplaneCircleFill = "airplane.circle.fill"
-    public static let starFill = "star.fill"
-    public static let squareGrid = "square.grid.2x2"
-    public static let ellipsis = "ellipsis"
-    public static let circle = "circle"
+    public static let checkmarkSeal = Image(systemName: "checkmark.seal")
+    public static let checkmarkSealFill = Image(systemName: "checkmark.seal.fill")
+    public static let pencilAndListClipboard = Image(systemName: "pencil.and.list.clipboard")
+    public static let minusCircle = Image(systemName: "minus.circle")
+    public static let docBadgeClock = Image(systemName: "doc.badge.clock")
+    public static let checklist = Image(systemName: "checklist")
+    public static let giftFill = Image(systemName: "gift.fill")
+    public static let carFill = Image(systemName: "car.fill")
+    public static let crossCircleFill = Image(systemName: "cross.circle.fill")
+    public static let plusCircle = Image(systemName: "plus.circle")
+    public static let plus = Image(systemName: "plus")
+    public static let magnifyingGlass = Image(systemName: "magnifyingglass")
+    public static let arrowLeft = Image(systemName: "arrow.left")
+    public static let line3Horizontal = Image(systemName: "line.3.horizontal")
+    public static let airplaneCircleFill = Image(systemName: "airplane.circle.fill")
+    public static let starFill = Image(systemName: "star.fill")
+    public static let squareGrid = Image(systemName: "square.grid.2x2")
+    public static let ellipsis = Image(systemName: "ellipsis")
+    public static let circle = Image(systemName: "circle")
+    public static let paperclip = Image(systemName: "paperclip")
+    public static let calendar = Image(systemName: "calendar")
+    public static let stopwatchFill = Image(systemName: "stopwatch.fill")
+    public static let clockBadgeExclamationmarkFill = Image(systemName: "clock.badge.exclamationmark.fill")
+    public static let clockArrowCirclepath = Image(systemName: "clock.arrow.circlepath")
+    public static let tagFill = Image(systemName: "tag.fill")
 }
