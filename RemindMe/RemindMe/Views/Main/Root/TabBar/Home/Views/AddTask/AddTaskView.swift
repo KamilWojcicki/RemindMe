990dc01f098a66e48eb7acb2deee768385571b92
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2024-09-25 12:05:56 +0200

    Add a AddTaskView

--- RemindMe/RemindMe/Views/Main/Root/TabBar/Home/Views/AddTask/AddTaskView.swift
+++ RemindMe/RemindMe/Views/Main/Root/TabBar/Home/Views/AddTask/AddTaskView.swift
@@ -5,11 +5,116 @@
 //  Created by Kamil Wójcicki on 10/08/2024.
 //
 
+import Components
+import Design
 import SwiftUI
 
 struct AddTaskView: View {
+    @StateObject private var viewModel = AddTaskViewModel()
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottom) {
+            ReadableScrollView(content: {
+                VStack(spacing: 15) {
+                    Text("New Task")
+                    
+                    Row(
+                        text: viewModel.newTaskTitle,
+                        variant: .title(
+                            icon: viewModel.newTaskSymbol,
+                            instruction: "Tap to rename and change the image"
+                        )
+                    ) {
+                        
+                    }
+                    
+                    Row(
+                        text: "Today",
+                        variant: .plainText(
+                            symbol: Symbols.calendar
+                        )
+                    ) {
+                        
+                    }
+                    
+                    Row(
+                        text: "Time: \(viewModel.taskTime)",
+                        variant: .plainText(
+                            symbol: Symbols.stopwatchFill
+                        )
+                    ) {
+                        
+                    }
+                    
+                    Row(
+                        text: "Remind: \(viewModel.remindTime)",
+                        variant: .plainText(
+                            symbol: Symbols.clockBadgeExclamationmarkFill
+                        )
+                    ) {
+                        
+                    }
+                    
+                    Row(
+                        text: "\(viewModel.repetition.description)",
+                        variant: .plainText(
+                            symbol: Symbols.clockArrowCirclepath
+                        )
+                    ) {
+                        
+                    }
+                    
+                    Row(
+                        text: "No tag",
+                        variant: .plainText(
+                            symbol: Symbols.tagFill
+                        )
+                    ) {
+                        
+                    }
+                    
+                    Row(
+                        text: "Subtask",
+                        variant: .subtask(
+                            symbol: Symbols.plus
+                        )) {
+                            
+                        }
+                    
+                    PhotoAttacher(defaultScrollAnchor: $viewModel.defaultScrollAnchor) {
+                        //action/attach photo/open photo library
+                    } deletePhotoAction: {
+                        //delete action
+                    }
+                }
+                .padding()
+            }, onScroll: { position in
+                viewModel.onScrollDividerAction()
+            })
+            .scrollIndicators(.hidden)
+            .defaultScrollAnchor(viewModel.defaultScrollAnchor)
+            .safeAreaInset(edge: .bottom,spacing: 0) {
+                VStack(spacing: 0) {
+                    if viewModel.showDivider {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Colors.night.opacity(0.1))
+                            .frame(height: 2)
+                            .frame(maxWidth: .infinity)
+                    }
+                        
+                        ConfirmButton(title: "Create Task") {
+                            dismiss()
+                        }
+                        .padding(.horizontal)
+                        .padding(.bottom, 30)
+                        .padding(.top, 10)
+                        .background(Colors.ghostWhite)
+                }
+                
+                }
+        }
+        .background(Colors.ghostWhite)
     }
 }
 
--- RemindMe/RemindMe/Views/Main/Root/TabBar/Home/Views/AddTask/AddTaskViewModel.swift
+++ RemindMe/RemindMe/Views/Main/Root/TabBar/Home/Views/AddTask/AddTaskViewModel.swift
@@ -5,4 +5,52 @@
 //  Created by Kamil Wójcicki on 17/08/2024.
 //
 
+import Components
+import Design
 import Foundation
+import SwiftUI
+
+enum Repetition {
+    case noRepeat
+    case minutely(String)
+    case hourly(String)
+    case daily(String, Date)
+    case weekly(String)
+    case monthly(String)
+    case yearly(String)
+    
+    var description: String {
+        switch self {
+        case .noRepeat:
+            "No repetition"
+        case .minutely(let minutes):
+            "Repetition every \(minutes) minutes"
+        case .hourly(let hour):
+            "Repetition every \(hour) hours"
+        case .daily(let day, let time):
+            "Repetition every \(day) at \(time)"
+        case .weekly(let week):
+            "Repetition every \(week)"
+        case .monthly(let month):
+            "Repetition every \(month)"
+        case .yearly(let year):
+            "Repetition every \(year)"
+        }
+    }
+}
+
+final class AddTaskViewModel: ObservableObject {
+    @Published var newTaskTitle: String = "Title task"
+    @Published var newTaskSymbol: Icon = .clipboardIcon
+    @Published var taskTime: String = "7:00 PM"
+    @Published var remindTime: String = "6:00 PM"
+    @Published var repetition: Repetition = .noRepeat
+    @Published var defaultScrollAnchor: UnitPoint? = .top
+    @Published private(set) var showDivider: Bool = true
+    
+    func onScrollDividerAction() {
+        withAnimation {
+            showDivider.toggle()
+        }
+    }
+}
