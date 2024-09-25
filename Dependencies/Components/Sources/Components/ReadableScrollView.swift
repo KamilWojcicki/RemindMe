9283c7931e66368275230ed8a94482bdef632644
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2024-09-25 11:42:45 +0200

    Add ReadableScrollView component to read a current CGPoint while scrolling

---
+++ Dependencies/Components/Sources/Components/ReadableScrollView.swift
@@ -0,0 +1,42 @@
+//
+//  ReadableScrollView.swift
+//  Components
+//
+//  Created by Kamil Wójcicki on 25/09/2024.
+//
+
+import SwiftUI
+
+public struct ReadableScrollView<Content>: View where Content: View {
+    let axes: Axis.Set
+    let content: () -> Content
+    let onScroll: (CGFloat) -> Void
+    
+    public init(axes: Axis.Set = .vertical, content: @escaping () -> Content, onScroll: @escaping (CGFloat) -> Void) {
+        self.axes = axes
+        self.content = content
+        self.onScroll = onScroll
+    }
+    
+    public var body: some View {
+        ScrollView(axes) {
+            content()
+                .background(
+                    GeometryReader { proxy in
+                        let position = (
+                            axes == .vertical ?
+                            proxy.frame(in: .named("scrollID")).origin.y :
+                            proxy.frame(in: .named("scrollID")).origin.x
+                        )
+                        
+                        Color.clear
+                            .onChange(of: position) { position, _ in
+                                onScroll(position)
+                            }
+                    }
+                )
+        }
+        .coordinateSpace(.named("scrollID"))
+    }
+}
+
