16f68a56e2004a7542a1f310783b181ebfc2dbd0
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2024-10-28 15:09:09 +0100

    Modify AddTaskView and add full functionality to create task

--- RemindMe/RemindMe/Views/Main/Root/TabBar/Home/Views/AddTask/AddTaskView.swift
+++ RemindMe/RemindMe/Views/Main/Root/TabBar/Home/Views/AddTask/AddTaskView.swift
@@ -7,17 +7,154 @@
 
 import Components
 import Design
+import Navigation
 import SwiftUI
+import Utilities
+
+
 
 struct AddTaskView: View {
     @StateObject private var viewModel = AddTaskViewModel()
     @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-        ZStack(alignment: .bottom) {
+        VStack(spacing: 0) {
+            switch viewModel.state {
+            case .idle:
+                EmptyView()
+            case .loading:
+                CustomProgressView(message: "Loading...")
+            case .loaded:
+                buildReadableScrollViewContent
+            case .error(let error):
+                Label(error.description, systemImage: "xmark.circle")
+                    .background(Color.red)
+            }
+            
+            bottomSpaceWithButton
+        }
+        .navigationBarBackButtonHidden()
+        .background(Colors.ghostWhite)
+        .withAlert(
+            errorTitle: "Warning",
+            message: "This function isn't available yet.",
+            errorToggle: $viewModel.alertToggle,
+            buttons: [
+                .submit(
+                    title: "Ok",
+                    action: {
+                        viewModel.trigger(.handleAlertToggle)
+                    }
+                )
+            ]
+        )
+    }
+}
+
+#Preview {
+    AddTaskView()
+}
+
+extension AddTaskView {
+    private var picker: some View {
+        VStack(spacing: 30) {
+            switch viewModel.selectedPicker {
+            case .title:
+                Picker(
+                    variant: .titleAndImage(textFieldText: $viewModel.newTaskTitle, selectedIcon: $viewModel.newTaskSymbol)
+                )
+            case .date:
+                Picker(
+                    variant: .time(
+                        selection: $viewModel.selectedDate,
+                        dateComponents: .date
+                    )
+                )
+            case .time:
+                Picker(
+                    variant: .time(
+                        selection: $viewModel.taskTime,
+                        dateComponents: .hourAndMinute
+                    )
+                )
+            case .reminder:
+                Picker(
+                    variant: .time(
+                        selection: Binding(
+                            get: {
+                                viewModel.remindTime.date ?? Date()
+                            },
+                            set: { newDate in
+                                viewModel.remindTime
+                                    .setDate(newDate)
+                            }),
+                        dateComponents: .hourAndMinute
+                    )
+                )
+            case .repetition:
+                Picker(variant: .repetition(selectedRepetition: $viewModel.repetition))
+            case .tag:
+                Picker(variant: .tag(selectedTag: $viewModel.tag))
+            case .subtask:
+                VStack { }
+            case .none:
+                EmptyView()
+            }
+        }
+    }
+    
+    private var bottomSpaceWithButton: some View {
+        ZStack {
+            VStack(spacing: 0) {
+                if viewModel.showDivider {
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Colors.night.opacity(0.1))
+                        .frame(height: 2)
+                        .frame(maxWidth: .infinity)
+                }
+                
+                if viewModel.isPickerSelected.wrappedValue {
+                    picker
+                        .transition(.asymmetric(insertion: .push(from: .bottom), removal: .identity))
+                }
+                
+                ConfirmButton(title: viewModel.isPickerSelected.wrappedValue ? "Confirm" : "Create Task", role: .confirm) {
+                    if viewModel.isPickerSelected.wrappedValue {
+                        viewModel.trigger(.onShowOptionToggle)
+                    } else {
+                        viewModel.trigger(.createToDo)
+                        dismiss()
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.top, 10)
+                .background(Colors.ghostWhite)
+            }
+        }
+    }
+    
+    private var buildReadableScrollViewContent: some View {
+        VStack(spacing: 0) {
+            ZStack {
+                Symbols.chevronBackward
+                    .padding()
+                    .onTapGesture {
+                        dismiss()
+                        print("dismiss")
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .tint(Colors.night)
+                
+                Text("New Task")
+            }
+            .font(.size18Default)
+            .frame(maxWidth: .infinity)
+            .background(viewModel.isScrolled ? Colors.night.opacity(0.08) : .clear)
+            .animation(.smooth, value: viewModel.isScrolled)
+            
             ReadableScrollView(content: {
                 VStack(spacing: 15) {
-                    Text("New Task")
+                    
                     
                     Row(
                         text: viewModel.newTaskTitle,
@@ -26,34 +163,34 @@ struct AddTaskView: View {
                             instruction: "Tap to rename and change the image"
                         )
                     ) {
-                        
+                        viewModel.trigger(.handlePickerSelection(.title))
                     }
                     
                     Row(
-                        text: "Today",
+                        text: "\(viewModel.taskDay.description)",
                         variant: .plainText(
                             symbol: Symbols.calendar
                         )
                     ) {
-                        
+                        viewModel.trigger(.handlePickerSelection(.date))
                     }
                     
                     Row(
-                        text: "Time: \(viewModel.taskTime)",
+                        text: "Time: \(dateFormatter(dateFormat: .time).string(from: viewModel.taskTime))",
                         variant: .plainText(
                             symbol: Symbols.stopwatchFill
                         )
                     ) {
-                        
+                        viewModel.trigger(.handlePickerSelection(.time))
                     }
                     
                     Row(
-                        text: "Remind: \(viewModel.remindTime)",
+                        text: "\(viewModel.remindTime.description)",
                         variant: .plainText(
                             symbol: Symbols.clockBadgeExclamationmarkFill
                         )
                     ) {
-                        
+                        viewModel.trigger(.handlePickerSelection(.reminder))
                     }
                     
                     Row(
@@ -62,62 +199,40 @@ struct AddTaskView: View {
                             symbol: Symbols.clockArrowCirclepath
                         )
                     ) {
-                        
+                        viewModel.trigger(.handlePickerSelection(.repetition))
                     }
                     
                     Row(
-                        text: "No tag",
+                        text: "\(viewModel.tag.rawValue)",
                         variant: .plainText(
                             symbol: Symbols.tagFill
                         )
                     ) {
-                        
+                        viewModel.trigger(.handlePickerSelection(.tag))
                     }
                     
                     Row(
                         text: "Subtask",
                         variant: .subtask(
                             symbol: Symbols.plus
-                        )) {
-                            
-                        }
-                    
-                    PhotoAttacher(defaultScrollAnchor: $viewModel.defaultScrollAnchor) {
-                        //action/attach photo/open photo library
-                    } deletePhotoAction: {
-                        //delete action
+                        )
+                    ) {
+                        viewModel.trigger(.handleAlertToggle)
+                        #warning("This function is not implemented yet")
+                        //viewModel.trigger(.handlePickerSelection(.subtask))
                     }
+                    
+                    PhotoAttacher(defaultScrollAnchor: $viewModel.defaultScrollAnchor, photoPickerSelection: $viewModel.taskImageSelection)
                 }
                 .padding()
             }, onScroll: { position in
-                viewModel.onScrollDividerAction()
+                viewModel.trigger(.onScrollDividerAction(position))
+                viewModel.trigger(.onScrolledHeaderAction(position))
             })
             .scrollIndicators(.hidden)
             .defaultScrollAnchor(viewModel.defaultScrollAnchor)
-            .safeAreaInset(edge: .bottom,spacing: 0) {
-                VStack(spacing: 0) {
-                    if viewModel.showDivider {
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(Colors.night.opacity(0.1))
-                            .frame(height: 2)
-                            .frame(maxWidth: .infinity)
-                    }
-                        
-                        ConfirmButton(title: "Create Task") {
-                            dismiss()
-                        }
-                        .padding(.horizontal)
-                        .padding(.bottom, 30)
-                        .padding(.top, 10)
-                        .background(Colors.ghostWhite)
-                }
-                
-                }
+            .disabled(viewModel.isPickerSelected.wrappedValue)
         }
-        .background(Colors.ghostWhite)
+        
     }
-}
-
-#Preview {
-    AddTaskView()
 }
--- RemindMe/RemindMe/Views/Main/Root/TabBar/Home/Views/AddTask/AddTaskViewModel.swift
+++ RemindMe/RemindMe/Views/Main/Root/TabBar/Home/Views/AddTask/AddTaskViewModel.swift
@@ -6,51 +6,175 @@
 //
 
 import Components
+import CoreInterface
+import DependencyInjection
 import Design
 import Foundation
+import PhotosUI
 import SwiftUI
+import ToDoInterface
+import Utilities
 
-enum Repetition {
-    case noRepeat
-    case minutely(String)
-    case hourly(String)
-    case daily(String, Date)
-    case weekly(String)
-    case monthly(String)
-    case yearly(String)
+@MainActor
+final class AddTaskViewModel: ViewModelInterface {
+    enum State: Equatable {
+        case idle
+        case loading
+        case loaded
+        case error(String)
+    }
     
-    var description: String {
-        switch self {
-        case .noRepeat:
-            "No repetition"
-        case .minutely(let minutes):
-            "Repetition every \(minutes) minutes"
-        case .hourly(let hour):
-            "Repetition every \(hour) hours"
-        case .daily(let day, let time):
-            "Repetition every \(day) at \(time)"
-        case .weekly(let week):
-            "Repetition every \(week)"
-        case .monthly(let month):
-            "Repetition every \(month)"
-        case .yearly(let year):
-            "Repetition every \(year)"
+    enum Event {
+        case onScrollDividerAction(Double)
+        case onScrolledHeaderAction(Double)
+        case onShowOptionToggle
+        case handlePickerSelection(Picker)
+        case handleAlertToggle
+        case createToDo
+    }
+    
+    @Published private(set) var state: State = .idle
+    @Published private(set) var showDivider: Bool = true
+    @Published private(set) var selectedPicker: Picker?
+    @Published private(set) var selectedTaskImage: UIImage? = nil
+    @Published var taskImageSelection: PhotosPickerItem? = nil {
+        didSet {
+            setImage(from: taskImageSelection)
         }
     }
+    //TODO: add validation to newTaskTitle
+    @Published var newTaskTitle: String = ""
+    @Published var isScrolled: Bool = false
+    @Published var newTaskSymbol: Icon = .clipboardIcon
+    @Published private(set) var taskDay: Day = .today
+    @Published var taskTime: Date = .now
+    @Published var remindTime: Reminder = .noReminder
+    @Published var repetition: Repetition = .noRepeat
+    @Published var tag: Tag = .all
+    @Published var defaultScrollAnchor: UnitPoint? = .top
+    @Published var alertToggle: Bool = false
+    @Inject private var toDoManager: ToDoManagerInterface
+    
+    var isPickerSelected: Binding<Bool> {
+        Binding<Bool>(
+            get: {
+                self.selectedPicker != nil },  // Return true if a picker is selected
+            set: {
+                if !$0 {
+                    self.selectedPicker = nil   // Set to nil if dismissed
+                }
+            }
+        )
+    }
+    
+    var selectedDate: Date {
+        get {
+            switch taskDay {
+            case .today:
+                return Date()
+            case .otherDay(let date):
+                return date
+            }
+        }
+        set {
+            if Calendar.current.isDateInToday(newValue) {
+                taskDay = .today
+            } else {
+                taskDay = .otherDay(newValue)
+            }
+        }
+    }
+    
+    init() {
+        self.state = .loaded
+    }
+    
+    func trigger(_ event: Event) {
+        switch event {
+        case .onScrollDividerAction(let position):
+            onScrollDividerAction(position: position)
+        case .onScrolledHeaderAction(let position):
+            onScrolledHeaderAction(position: position)
+        case .onShowOptionToggle:
+            onShowOptionToggle()
+        case .handlePickerSelection(let picker):
+            handlePickerSelection(picker: picker)
+        case .handleAlertToggle:
+            handleAlertToggle()
+        case .createToDo:
+            createToDo()
+        }
+    }
+    
+    private func onScrollDividerAction(position: Double) {
+        showDivider = position < -80.0 ? false : true
+    }
+    
+    private func onScrolledHeaderAction(position: Double) {
+        isScrolled = position < -10
+    }
+    
+    private func onShowOptionToggle() {
+        withAnimation(.smooth(duration: 0.1)) {
+            selectedPicker = nil
+        }
+    }
+    
+    private func handlePickerSelection(picker: Picker) {
+        withAnimation(.smooth(duration: 0.7)) {
+            selectedPicker = picker
+        }
+    }
+    
+    private func handleAlertToggle() {
+        withAnimation {
+            alertToggle.toggle()
+        }
+    }
+    
+    typealias Picker = ToDoInterface.Picker
 }
 
-final class AddTaskViewModel: ObservableObject {
-    @Published var newTaskTitle: String = "Title task"
-    @Published var newTaskSymbol: Icon = .clipboardIcon
-    @Published var taskTime: String = "7:00 PM"
-    @Published var remindTime: String = "6:00 PM"
-    @Published var repetition: Repetition = .noRepeat
-    @Published var defaultScrollAnchor: UnitPoint? = .top
-    @Published private(set) var showDivider: Bool = true
+extension AddTaskViewModel {
+    private func setImage(from selection: PhotosPickerItem?) {
+        guard let selection else { return }
+        
+        Task {
+            do {
+                let data = try await selection.loadTransferable(type: Data.self)
+                
+                guard let data, let uiImage = UIImage(data: data) else {
+                    //TODO: throw custom error
+                    return
+                }
+                
+                selectedTaskImage = uiImage
+            } catch {
+                print(error)
+            }
+        }
+    }
     
-    func onScrollDividerAction() {
-        withAnimation {
-            showDivider.toggle()
+    private func createToDo() {
+        Task {
+            do {
+                let newToDo = ToDo(
+                    name: newTaskTitle,
+                    symbol: newTaskSymbol,
+                    image: selectedTaskImage?.jpegData(compressionQuality: 0.9),
+                    executedDate: taskDay,
+                    executedTime: taskTime,
+                    remindTime: remindTime,
+                    reminderRepetition: repetition,
+                    tag: tag
+                )
+                
+                try await toDoManager.createToDo(todo: newToDo)
+                
+                debugPrint("success saving todo!!!!!")
+            } catch {
+                print(error.localizedDescription)
+            }
         }
     }
 }
