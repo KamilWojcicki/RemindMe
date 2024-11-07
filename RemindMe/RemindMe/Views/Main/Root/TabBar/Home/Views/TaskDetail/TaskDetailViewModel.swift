e5c74b2e51e5e777b72c09af5b0836e8a5347514
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2024-11-07 15:27:02 +0100

    Add TaskDetailView and ViewModel

--- RemindMe/RemindMe/Views/Main/Root/TabBar/Home/Views/TaskDetail/TaskDetailViewModel.swift
+++ RemindMe/RemindMe/Views/Main/Root/TabBar/Home/Views/TaskDetail/TaskDetailViewModel.swift
@@ -5,4 +5,20 @@
 //  Created by Kamil Wójcicki on 30/10/2024.
 //
 
+import DependencyInjection
 import Foundation
+import ToDoInterface
+import Utilities
+
+@MainActor
+final class TaskDetailViewModel: ObservableObject {
+    @Inject private var toDoManager: ToDoManagerInterface
+    
+    func updateSubtask(task: ToDo, subtask: SubToDo) async throws {
+        var updatedSubToDo = subtask
+        updatedSubToDo.isCompleted.toggle()
+        let updates = compare(old: subtask, updated: updatedSubToDo)
+        
+        try await toDoManager.updateSubToDo(task: task, subToDo: subtask, data: updates)
+    }
+}
