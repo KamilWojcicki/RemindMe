a6a5b2734fdf6f63c22b0a68b6b8bbc79453319a
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2024-11-07 15:26:13 +0100

    Modify TaskInfoCellView, add functionality to update ToDo

--- RemindMe/RemindMe/Views/Main/Root/TabBar/Home/Components/TaskInfoCell/TaskInfoCellViewModel.swift
+++ RemindMe/RemindMe/Views/Main/Root/TabBar/Home/Components/TaskInfoCell/TaskInfoCellViewModel.swift
@@ -5,4 +5,28 @@
 //  Created by Kamil Wójcicki on 02/11/2024.
 //
 
+import DependencyInjection
 import Foundation
+import ToDoInterface
+import Utilities
+
+final class TaskInfoCellViewModel: ObservableObject {
+    @Inject private var toDoManager: ToDoManagerInterface
+    
+    
+    func updateTask(task: ToDo) {
+        Task {
+            do {
+                var updatedToDo = task
+                
+                updatedToDo.isDone.toggle()
+                
+                let updates = compare(old: task, updated: updatedToDo)
+            
+                try await toDoManager.updateToDo(todo: task, data: updates)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
