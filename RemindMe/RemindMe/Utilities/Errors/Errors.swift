d3b72626eaee56e32dbbe02e166d83a86da1ca3c
Kamil Wójcicki <kamilwojcickinjr@gmail.com>
2024-12-03 00:27:05 +0100

    Add Errors file to app

--- RemindMe/RemindMe/Utilities/Errors/Errors.swift
+++ RemindMe/RemindMe/Utilities/Errors/Errors.swift
@@ -11,22 +11,25 @@ enum AppError: LocalizedError {
     case unableToFetchToDos
     case unableToCreateToDo
     case unableToLoadImage
-    case unableToUpdateSubtask
+    case unableToUpdateSubToDo
     case unableToUpdateToDo
+    case unableToCreateEmptyTitle
     case unexpectedError(String)
     
     var errorDescription: String? {
         switch self {
         case .unableToFetchToDos:
-            return "Unable to fetch taskss"
+            return "Unable to fetch ToDo's"
         case .unableToCreateToDo:
-            return "Unable to create task"
+            return "Unable to create ToDo"
         case .unableToLoadImage:
             return "Unable to load image"
-        case .unableToUpdateSubtask:
-            return "Unable to update subtask"
+        case .unableToUpdateSubToDo:
+            return "Unable to update SubToDo"
         case .unableToUpdateToDo:
-            return "Unable to update task"
+            return "Unable to update ToDo"
+        case .unableToCreateEmptyTitle:
+            return "Title cannot be empty!"
         case .unexpectedError(let error):
             return "Unexpected error occured: \(error)"
         }
